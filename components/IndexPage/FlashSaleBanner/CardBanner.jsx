c9e5435fa69488ea6975270e97946597c9ba0248
HOANGNBD <Duchoang020120@gmail.com>
2023-10-26 23:16:56 +0700

    upodatep

--- components/IndexPage/FlashSaleBanner/CardBanner.jsx
+++ components/IndexPage/FlashSaleBanner/CardBanner.jsx
@@ -34,7 +34,7 @@ function CardBanner({ products }) {
         onNext={handleSlideNext}
       />
       <div className="relative mx-auto max-w-[1505px] ">
-        <div className="ml-16 2xl:ml-[168px] max-w-[1308px]">
+        <div className="ml-16  md:ml-[138px] 2xl:ml-[168px] sm:ml-0 max-w-[1308px]">
           {" "}
           <Swiper
             spaceBetween={10}
