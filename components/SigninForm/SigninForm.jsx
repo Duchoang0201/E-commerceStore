1884a21384092ae36058382b574b41c060021d34
HOANGNBD <Duchoang020120@gmail.com>
2023-10-30 13:45:30 +0700

    fix-bug

--- components/SigninForm/SigninForm.jsx
+++ components/SigninForm/SigninForm.jsx
@@ -9,7 +9,7 @@ const validationSchema = yup.object().shape({
     .required("Email or Phone Number is required")
     .matches(
       /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$|^(\+\d{1,3}[- ]?)?\d{10}$/,
-      "Invalid Email or Phone Number"
+      "Invalid Email or Phone Number",
     ),
   password: yup
     .string()
