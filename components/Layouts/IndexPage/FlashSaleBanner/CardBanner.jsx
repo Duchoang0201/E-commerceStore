489eb37f7a4683da0123e249f3485ddc758ba600
HOANGNBD <Duchoang020120@gmail.com>
2023-11-24 15:26:56 +0700

    update-css-flashBanner-complete

--- components/Layouts/IndexPage/FlashSaleBanner/CardBanner.jsx
+++ components/Layouts/IndexPage/FlashSaleBanner/CardBanner.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useCallback, useRef } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import Link from "next/link";
 import PropTypes from "prop-types";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -25,7 +25,7 @@ function CardBanner({ products }) {
   const handleSlideNext = useCallback(() => {
     swiperRef.current.slideNext();
   }, []);
-
+  const [hideSlide, setHideSlide] = useState(0);
   return (
     <div className="relative">
       <TitleFunction
@@ -38,8 +38,8 @@ function CardBanner({ products }) {
         buttonText="View All"
       />
 
-      <div className="container xxl:max-w-[1465px] mt-[40px]  ">
-        <div className="xxl:ml-[115px] ">
+      {/* <div className="container xxl:max-w-[1465px] mt-[40px] ">
+        <div className="xxl:ml-[105px] ">
           <Swiper
             watchSlidesProgress="true"
             onBeforeInit={(swiper) => {
@@ -105,6 +105,84 @@ function CardBanner({ products }) {
               })}
           </Swiper>
         </div>
+      </div> */}
+      <div className="container mx-auto w-full mt-[40px]">
+        <Swiper
+          watchOverflow="true"
+          watchSlidesProgress="true"
+          onBeforeInit={(swiper) => {
+            swiperRef.current = swiper;
+          }}
+          onSlideChange={() => {
+            setHideSlide((prev) => prev + 1);
+          }}
+          navigation={{
+            nextEl: ".review-swiper-button-next",
+            prevEl: ".review-swiper-button-prev",
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          className="mySwiper xxl:!overflow-visible "
+          breakpoints={{
+            1280: {
+              slidesPerView: "auto",
+              spaceBetween: 30,
+              // slidesOffsetBefore: 135,
+              slidesOffsetAfter: 115,
+            },
+
+            1010: {
+              slidesPerView: 4,
+
+              spaceBetween: 30,
+            },
+            680: {
+              slidesPerView: 3.5,
+              spaceBetween: 30,
+            },
+            480: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+            },
+            400: {
+              slidesPerView: 2.5,
+              spaceBetween: 30,
+            },
+            320: {
+              slidesPerView: 2,
+              spaceBetween: 30,
+            },
+          }}
+        >
+          {products.length > 0 &&
+            products.map((item, index) => {
+              return (
+                <SwiperSlide
+                  className="!max-w-[270px] !max-h-[350px]"
+                  key={item.id}
+                >
+                  <Link href={`/products/${item.id}`}>
+                    <ProductCart
+                      index={index}
+                      item={item}
+                      isEye={{ isActive: true }}
+                      isDiscount={{ isActive: true, value: 20 }}
+                      isHeart={{ isActive: true }}
+                    />
+                  </Link>
+                </SwiperSlide>
+              );
+            })}
+        </Swiper>
+        <style>
+          {`
+          .swiper-slide:nth-of-type(${5 + hideSlide}){
+            opacity: 1 !important;
+            filter: none;
+          
+          }`}
+        </style>
       </div>
     </div>
   );
