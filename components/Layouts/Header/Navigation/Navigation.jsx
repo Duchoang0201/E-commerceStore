c445448b6f4448decf7cf49f26f3d20c1ae0e592
HOANGNBD <Duchoang020120@gmail.com>
2023-11-27 15:03:57 +0700

    fix-navigation

--- components/Layouts/Header/Navigation/Navigation.jsx
+++ components/Layouts/Header/Navigation/Navigation.jsx
@@ -14,27 +14,23 @@ function Navigation() {
   const { navigationList } = useTrans();
   const router = useRouter();
   const { user: isUser } = useAuthStore();
-
   const isUserClass = `${
     router.pathname === "/signin" || router.pathname === "/signup"
       ? `max-w-[820px] w-full `
       : `w-full lg:max-w-[862px] ${
           isUser &&
           isUser?.name &&
-          `max-w-[440px] w-full lg:max-w-[892px] lg:justify-between lg:w-full`
+          `max-w-[440px] w-full lg:max-w-[892px]  lg:w-full`
         } `
   }`;
   return (
     <div className="pb-[14px] pt-[40px] border-b border-Neutral-200">
-      <div className="container flex flex-row justify-between  items-center">
-        <div className="xl:max-w-[118px] max-w-[125px]  w-full">
-          <Link
-            href="/"
-            className="font-inter w-full text-[24px] tracking-[0.72px] not-italic  !font-bold text-black  "
-          >
+      <div className="container flex flex-row justify-between items-center">
+        <Link href="/" className="xl:max-w-[118px] max-w-[125px]  w-full  ">
+          <span className="font-inter w-full text-[24px] tracking-[0.72px] not-italic  !font-bold text-black">
             Exclusive
-          </Link>
-        </div>
+          </span>
+        </Link>
 
         <div className={isUserClass}>
           <div className="flex flex-row justify-end gap-4  lg:flex lg:flex-row lg:justify-between w-full">
