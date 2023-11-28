985c2e85f6d2451896ba9ed74759a6e08c0b65e5
HOANGNBD <Duchoang020120@gmail.com>
2023-11-28 09:19:49 +0700

    update-performance

--- components/Commons/ProductCard.jsx
+++ components/Commons/ProductCard.jsx
@@ -31,7 +31,8 @@ function ProductCard({ item, isHeart, isEye, isDiscount }) {
             e.preventDefault();
             addCart(item);
           }}
-          className="bg-black text-white-0 py-2 px-5 absolute h-10 w-full bg-black-0  flex items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300"
+          className="bg-black text-white-0 py-2 px-5 absolute h-10 w-full bg-black-0  flex items-center justify-center bottom-0 lg:-bottom-10 
+          lg:group-hover:bottom-0 lg:opacity-0 lg:group-hover:opacity-100 transition-all duration-300"
         >
           Add to cart
         </button>
