0ba5cbe25ab56b377f53e52d3db709a707a794d1
HOANGNBD <Duchoang020120@gmail.com>
2023-11-29 12:14:37 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import React from "react";
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 import PropTypes from "prop-types";
 
-import CircleLoading from "@/components/App/CircleLoading/CircleLoading";
-// import AppAlert from "@/components/App/AppAlert/AppAlert";
-// import MoveTop from "@/components/App/AppMoveTop/MoveTop";
-// import PhotoPreview from "@/components/App/AppPhotoView/PhotoPreview";
+import AppAlert from "@/components/App/AppAlert/AppAlert";
+import MoveTop from "@/components/App/AppMoveTop/MoveTop";
+import PhotoPreview from "@/components/App/AppPhotoView/PhotoPreview";
+// import CircleLoading from "@/components/App/CircleLoading/CircleLoading";
 import Footer from "@/components/Layouts/Footer/Footer";
 import Header from "@/components/Layouts/Header/Header";
 
@@ -19,28 +19,28 @@ import "@/styles/globals.css";
 //   ssr: false,
 //   // loading: lazy,
 // });
-const MoveTop = dynamic(() => import("@/components/App/AppMoveTop/MoveTop"), {
-  ssr: false,
-  loading: () => {
-    <CircleLoading />;
-  },
-});
-const AppAlert = dynamic(() => import("@/components/App/AppAlert/AppAlert"), {
-  ssr: false,
-  loading: () => {
-    <CircleLoading />;
-  },
-});
+// const MoveTop = dynamic(() => import("@/components/App/AppMoveTop/MoveTop"), {
+//   ssr: false,
+//   loading: () => {
+//     <CircleLoading />;
+//   },
+// });
+// const AppAlert = dynamic(() => import("@/components/App/AppAlert/AppAlert"), {
+//   ssr: false,
+//   loading: () => {
+//     <CircleLoading />;
+//   },
+// });
 
-const PhotoPreview = dynamic(
-  () => import("@/components/App/AppPhotoView/PhotoPreview"),
-  {
-    ssr: false,
-    loading: () => {
-      <CircleLoading />;
-    },
-  },
-);
+// const PhotoPreview = dynamic(
+//   () => import("@/components/App/AppPhotoView/PhotoPreview"),
+//   {
+//     ssr: false,
+//     loading: () => {
+//       <CircleLoading />;
+//     },
+//   },
+// );
 export default function App({ Component, pageProps }) {
   return (
     <div className="font-poppins">
